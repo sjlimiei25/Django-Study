2ac404f292e0cce2d4c6a49cbf0f029b4d55984e
sjlimiei25 <sjlim.iei.25@gmail.com>
2025-11-10 10:03:30 +0900

    상품 추가, 상세피이지 처리

--- viewpractice/funcview/urls.py
+++ viewpractice/funcview/urls.py
@@ -6,5 +6,6 @@ app_name = 'funcview'
 # 기본 요청 주소 : http://.../fv/
 urlpatterns = [
   path('', views.index, name='index'),
-  path('new/', views.new_product, name='new_product')
+  path('new/', views.new_product, name='new_product'),
+  path('detail/', views.detail_product, name='detail')
 ]
--- viewpractice/funcview/views.py
+++ viewpractice/funcview/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Product
 
@@ -12,6 +12,23 @@ def new_product(request):
     return render(request, 'funcview/new_product.html')
   elif request.method == 'POST':
     # 요청 시 전달된 데이터 추출
+    name = request.POST.get('name')
+    price = request.POST.get('price')
+    stock = request.POST.get('stock')
+    desc = request.POST.get('desc')
+
     # DB에 저장(추가)
+    Product.objects.create(name=name, price=price, stock=stock, desc=desc)
+
     # 목록페이지로 이동
-    pass
+    return redirect('funcview:index')
+    
+def detail_product(request):
+  # 요청 시 전달된 데이터 추출
+  #  - 요청 방식 : GET
+  id = request.GET.get('id')
+
+  # 해당 값으로 조회
+  product = Product.objects.get(id=id)
+
+  return render(request, 'funcview/detail_product.html', {'product': product})
